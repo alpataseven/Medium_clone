c617de12678fd3c469549cabbde05bd3f5c72b9c
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2024-06-12 02:12:08 +0300

    login oluşturuldu...

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def login_view(request):
+    if request.user.is_authenticated:
+        messages.info(request, f'{request.user.username} daha önceden giriş yapmışsınız..')
+        return redirect('home_view')
     context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'{request.user.username} giriş yapıldı..')
+            return redirect('home_view')
     return render(request, 'user_profile/login.html', context)
